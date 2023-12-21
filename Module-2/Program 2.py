ffe55c1f62df5efc898e93f0ddc99706425110ce
Riktamaster <riktajadav@gmail.com>
2023-12-21 18:50:12 +0530

    Program 2 added

---
+++ Module-2/Program 2.py
@@ -0,0 +1,15 @@
+# Write a Python program to get factorial of a given number
+
+#To get a number from the user
+n=int(input("Enter a number:"))
+
+#To find the factorial of the number entered
+if n<0:
+    print("Factorial is not defined for nragtive number.")
+elif n==0:
+    print("Factorial of 0 is 1.")
+else:
+    fact=1
+    for i in range(1,n+1):
+        fact *=i
+    print(f"The factorial of {n} is {fact}.")
