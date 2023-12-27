654cb0e1eae78c20d0db237ad332af19d8dce5b8
Riktamaster <riktajadav@gmail.com>
2023-12-27 17:18:32 +0530

    Program added

--- Module-1 Programs/Program 18.py
+++ Module-1 Programs/Program 18.py
@@ -6,7 +6,10 @@ mystr=input("Enter list of words: ")
 #To split the list of words into single word
 words=mystr.split()
 
-#To find the length of the longest word
+#To find the longest word from the string
 longest_word=max(words, key=len)
+
+#To find the length of the longest word
 length=len(longest_word)
+
 print(f"Length of {longest_word} is {length}")
---
+++ Module-1 Programs/Program 19_a.py
@@ -0,0 +1,13 @@
+# Write a Python function to reverses a string if its length is a multiple of 4
+
+#To get string from the user
+mystr=input("Enter string: ")
+
+#To reverse the entered string using string slicing
+rev_mystr=mystr[::-1]
+
+#To print the reversed string if its length is a multiple of 4
+if len(rev_mystr)%4==0:
+    print(rev_mystr)
+else:
+    print(mystr)
