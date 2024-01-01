546a96c29c939a1b768b36bda8f225fd8aec607b
Riktamaster <riktajadav@gmail.com>
2024-01-01 21:44:00 +0530

    Program added

--- Module-2 Programs/Program 15.py
+++ Module-2 Programs/Program 15.py
@@ -1,10 +1,13 @@
 # Write a Python program to create a tuple with numbers
 
+# Define data as list
 data=[]
 
+# To get input for list from user
 n=int(input("Enter number of elements: "))
 for i in range(n):
     x=input(f"Enter element {i+1}: ")
     data.append(x)
-
+    
+# To print data as tuple
 print("Data:",tuple(data))
