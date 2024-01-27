2477fc55f85595c946849768f6d1b8e32fc957f7
Riktamaster <142592594+Riktamaster@users.noreply.github.com>
2024-01-27 13:55:28 +0530

    Update Program12.py

--- Module-3 Programs/Program12.py
+++ Module-3 Programs/Program12.py
@@ -1,11 +1,12 @@
 '''Write python program that user to enter only odd numbers, 
 else will raise an exception.'''
 
-import sys
+
 try:
     n = int(input("Enter an odd number: "))
     if n % 2 != 0:
         print(f"You entered a valid odd number: {n}")
-        sys.exit()    
+    else:
+        raise ValueError
 except:
     print("Error: Please enter an odd number.")
